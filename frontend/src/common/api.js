b45ff307526050266938f3060d0b07a317e1ae0f
vinitpatel135 <124019126+vinitpatel135@users.noreply.github.com>
2024-10-15 16:45:08 +0530

    Update api.js

--- frontend/src/common/api.js
+++ frontend/src/common/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API = axios.create({ baseURL: 'https://team-collobration-hub.onrender.com/api' ,  });
-const API = axios.create({ baseURL: 'http://localhost:5000/api' ,  });
+const API = axios.create({ baseURL: 'https://team-collab-hub.onrender.com/api' ,  });
+// const API = axios.create({ baseURL: 'http://localhost:5000/api' ,  });
 
 export const registerUser = async (userData) => {
   return await API.post('/users/register', userData);
